1be4d93044e7b3e181f194ce9972560e6c87a9f1
LuisHerlan7 <luis321herlan@gmail.com>
2025-11-20 22:34:00 -0400

    arreglo de los cores

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,10 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => CheckRole::class,
         ]);
 
-        // (opcional) si quieres forzar JSON en /api/*
-        // $middleware->appendToGroup('api', [
-        //     ForceJsonResponseForApi::class,
-        // ]);
+        // 🌐 Configurar CORS explícitamente para Laravel 11
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
